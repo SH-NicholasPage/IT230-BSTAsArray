8ac08a0ee1db3490213170f6a82a5dda2f56e3d9
Nicholas Page <npage@southhills.edu>
2023-11-12 17:37:55 -0500

    Initial commit

---
+++ BSTAsArray/BinarySearchTreeArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BSTAsArray
+{
+    public class BinarySearchTreeArray<T>
+        where T : IComparable<T>
+    {
+        public void Insert(T value)
+        {
+            //TODO
+        }
+
+        public void Delete(T value)
+        {
+            //TODO
+        }
+
+        public override String ToString()
+        {
+            //TODO
+            return base.ToString();
+        }
+    }
+}
---
+++ BSTAsArray/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace BSTAsArray
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            TestBSTArray();
+            //You can add additional test cases here
+        }
+
+        //DO NOT MODIFY THIS METHOD
+        private static void TestBSTArray()
+        {
+            BinarySearchTreeArray<int> bst = new BinarySearchTreeArray<int>();
+            bst.Insert(50);//Root
+            //Left subtree
+            bst.Insert(25);
+            bst.Insert(10);
+            bst.Insert(33);
+            bst.Insert(4);
+            bst.Insert(11);
+            bst.Insert(30);
+            bst.Insert(40);
+            //Right subtree
+            bst.Insert(75);
+            bst.Insert(56);
+            bst.Insert(89);
+            bst.Insert(52);
+            bst.Insert(61);
+            bst.Insert(82);
+            bst.Insert(95);
+            Console.WriteLine(bst);
+
+            Console.WriteLine("Starting tree...");
+            Console.WriteLine(bst);
+
+            doDeletion(bst, 4);
+            doDeletion(bst, 10);
+            doDeletion(bst, 56);
+
+            Console.WriteLine("Adding 55...");
+            bst.Insert(55);
+            Console.WriteLine(bst);
+
+            doDeletion(bst, 50);
+        }
+
+        //DO NOT MODIFY THIS METHOD
+        public static void doDeletion(BinarySearchTreeArray<int> bst, int value)
+        {
+            Console.WriteLine("\n===============================");
+            bst.Delete(value);
+            Console.WriteLine($"After deleting {value}...");
+            Console.WriteLine(bst);
+            Console.WriteLine("===============================\n");
+        }
+    }
+}
